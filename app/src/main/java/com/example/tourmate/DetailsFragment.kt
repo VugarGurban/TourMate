d14b27d7d159f1943ef574297063d46b158089c2
Vugar Gurbanov <vugar.gurbanov@outlook.com>
2025-03-21 16:54:00 +0400

    feat: Implement category item, details fragment, and enhance bottom navigation
    This commit introduces a category item layout, a new details fragment, and improves the bottom navigation bar.
    
    - **Category Item:**
        - Created `category_item.xml` to display category data.
        - Added `category_black_border.xml` to create a black border with rounded corners for the category item.
    - **Details Fragment:**
        - Created `DetailsFragment` with a basic layout in `fragment_details.xml`.
    - **Bottom Navigation:**
        - Modified `activity_main.xml` to add a background color and change item colors for the bottom navigation bar.
        - Added `bottom_nav_icon_color.xml` to manage the color of icons in the bottom navigation.
    - **Vector Drawables:**
        - Changed the stroke color of `ic_setting.xml` and `ic_favourites.xml` from #292D32 to #F9F9F9.
    - **Colors:**
        - Changed `secondary_grey` color to `#d8d8d8`.
    - **Other:**
        - Removed unused code in `HomeFragment`.
        - Added `fragment_details.xml`.

--- app/src/main/java/com/example/tourmate/DetailsFragment.kt
+++ app/src/main/java/com/example/tourmate/DetailsFragment.kt
@@ -6,21 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class HomeFragment : Fragment() {
-
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (activity as NavigationListener).changeBottomMenuVisibility(View.VISIBLE)
-    }
 }
