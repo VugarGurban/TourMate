13f9e986042635a6cf1d7219d7582fef61b756b6
Vugar Gurbanov <vugar.gurbanov@outlook.com>
2025-03-27 00:06:35 +0400

    feat: Implement Category Selection and Enhance Bottom Navigation
    This commit introduces category selection functionality, improves bottom navigation, and makes UI enhancements.
    
    - **Category Selection:**
        - Implemented `CategoryAdapter` to manage the display of categories in a RecyclerView.
        - Created `Category` enum class to define available categories.
        - Implemented category selection logic, including updating the UI when a category is selected.
        - Added `category_item.xml` layout to represent each category item.
        - Created `category_border_not_selected.xml` for the category border when not selected.
        - Created `category_border_selected.xml` for the category border when selected.
    - **Bottom Navigation:**
        - Replaced `bottom_nav_icon_color.xml` with direct color assignments.
        - Added `bottom_nav_home_selector.xml`, `bottom_nav_favourites_selector.xml`, and `bottom_nav_settings_selector.xml` to manage the icon state in the bottom navigation.
        - Added icons `ic_home_selected.xml`, `ic_settings_selected.xml`, `ic_favourites_selected.xml`, `ic_app_white.xml` and `ic_settings.xml`.
        - Modified `activity_main.xml` to improve bottom navigation appearance.
        - Changed the `item` title in `bottom_bar_menu.xml` from fixed text to `string` resources.
    - **UI Enhancements:**
        - Added a header to the `HomeFragment` with `CardView` and `ImageView`.
        - Added a guide line to separate the header from the RecyclerView.
        - Modified the `select_fragment.xml` `CardView`
        - Added dimens resources for category text size.
    - **Fragment:**
        - Added `RecyclerView` to `HomeFragment` to display categories.
    - **Colors:**
        - Added `colors.xml` in `values-night` folder to manage colors in dark theme.
    - **Other:**
        - Removed unused code.
        - Removed unused imports.
        - Fixed UI in `category_item.xml` layout.
        - Implemented `onCategoryClick` function in `HomeFragment` to display selected category.

---
+++ app/src/main/java/com/example/tourmate/Category.kt
@@ -0,0 +1,8 @@
+package com.example.tourmate
+
+enum class Category(val displayName: String) {
+    ALL("All"),
+    HISTORICAL("Historical"),
+    MUSEUMS("Museums"),
+    PUBLIC("Public")
+}
--- app/src/main/java/com/example/tourmate/HomeFragment.kt
+++ app/src/main/java/com/example/tourmate/HomeFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tourmate.adapters.CategoryAdapter
 
 
 class HomeFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +25,32 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as NavigationListener).changeBottomMenuVisibility(View.VISIBLE)
+
+        val categoryList: List<Category> = listOf(Category.ALL, Category.HISTORICAL, Category.MUSEUMS, Category.PUBLIC)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView1)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = CategoryAdapter(categoryList, { category -> onCategoryClick(category) })
+
     }
+
+    private fun onCategoryClick(category: Category) {
+        when (category) {
+            Category.ALL -> {
+                Toast.makeText(requireContext(), "All", Toast.LENGTH_SHORT).show()
+            }
+
+            Category.HISTORICAL -> {
+                Toast.makeText(requireContext(), "Historical", Toast.LENGTH_SHORT).show()
+            }
+
+            Category.MUSEUMS -> {
+                Toast.makeText(requireContext(), "Museums", Toast.LENGTH_SHORT).show()
+            }
+
+            Category.PUBLIC -> {
+                Toast.makeText(requireContext(), "Public", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
